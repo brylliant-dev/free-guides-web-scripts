4a09f2258346e729ef9c668ad5310a1ba0d668c3
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-04-17 11:45:18 +0800

    chore(): use typeof string

--- dev/compendium.js
+++ dev/compendium.js
@@ -174,7 +174,7 @@ const runFn = async () => {
                     writeMedia('media-website', `https://www.${medData.website}`, 'href')
                     writeMedia('media-maps-url', medData.mapsUrl || '#', 'href')
 
-                    if (!isJsonParseable(medData.cardImg)) {
+                    if (typeof medData.cardImg === 'string') {
                         writeMedia('media-card-img-1', medData.cardImg || '', 'src')
                         mediaClone.querySelector(`[recom-data="media-card-img-2"]`).style.opacity = '0'
                         mediaClone.querySelector(`[recom-data="media-card-img-3"]`).style.opacity = '0'
