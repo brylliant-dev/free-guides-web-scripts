70f24c2a4a23e83f62122eff7fd857572caff2e2
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-04-17 18:36:17 +0800

    chore(): revert back code to using img element

--- dev/compendium.js
+++ dev/compendium.js
@@ -161,16 +161,7 @@ const runFn = async () => {
                 // Update content with data from the array
                 recomDropdownTemplate.setAttribute('data-w-id', generateUUID())
                 recomDropdownClone.querySelector('[recom-data="accordion-title"]').textContent = data.accordionTitle;
-
-                const mediaIcon = recomDropdownClone.querySelector(`[recom-data="media-icon"]`)
-
-                if (data.iconDetails.url.split('.svg').length > 1) {
-                    const svgElem = `<object type="image/svg+xml" data="${data.iconDetails.url}"></object>`
-                    mediaIcon.innerHTML = svgElem
-                } else {
-                    const pngElem = `<img src="${data.iconDetails.url}" />`
-                    mediaIcon.innerHTML = pngElem
-                }
+                recomDropdownClone.querySelector(`[recom-data="media-icon"]`).setAttribute('src', data.iconDetails.url)
 
                 data.media.filter(med => med.active).forEach((medData, medIdx) => {
                     const mediaClone = recomCardWrapperTemplate.cloneNode(true);
@@ -304,16 +295,7 @@ const runFn = async () => {
                 // Update content with data from the array
                 genDropdownTemplate.setAttribute('data-w-id', generateUUID())
                 genDropdownClone.querySelector('[gen-data="accordion-title"]').textContent = data.accordionTitle;
-
-                const mediaIcon = genDropdownClone.querySelector(`[gen-data="media-icon"]`)
-
-                if (data.iconDetails.url.split('.svg').length > 1) {
-                    const svgElem = `<object type="image/svg+xml" data="${data.iconDetails.url}"></object>`
-                    mediaIcon.innerHTML = svgElem
-                } else {
-                    const pngElem = `<img src="${data.iconDetails.url}" />`
-                    mediaIcon.innerHTML = pngElem
-                }
+                genDropdownClone.querySelector(`[gen-data="media-icon"]`).setAttribute('src', data.iconDetails.url)
 
                 const genMediaTextTemplate = genDropdownClone.querySelector('[gen-data="media-text"]')
                 const genMediaLinkTemplate = genDropdownClone.querySelector('[gen-data="media-link"]').parentElement
