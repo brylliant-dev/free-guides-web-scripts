68abf67b0abca6b073ec766d45039b46e4ede681
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-04-23 17:14:33 +0800

    refactor(): reuse and rewrite functions

--- dev/compendium.js
+++ dev/compendium.js
@@ -1,4 +1,5 @@
 const generateUUID = () => {
+    // We're gonna need this for `data-w-id`
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
         const r = Math.random() * 16 | 0,
             v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -6,6 +7,7 @@ const generateUUID = () => {
     });
 }
 
+// We'll use this function to wait for certain query selectors before we run a callback
 const startObservingElements = ({ selectors, callback }) => {
     const observer = new MutationObserver((_mutations, obs) => {
         let foundSelectors = [];
@@ -35,11 +37,15 @@ const startObservingElements = ({ selectors, callback }) => {
     });
 };
 
+const truncateString = (str, maxLength) => {
+    return str.length <= maxLength ? str : str.slice(0, maxLength - 3) + '...';
+}
+
 const runFn = async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const idParam = urlParams.get('new');
-
     const isNew = idParam && idParam.includes('true')
+    // Check if the URL has `new=true` parameter
 
     const compendium = document.querySelector('code#json-compendium')
     const compendiumText = compendium.textContent
@@ -48,62 +54,35 @@ const runFn = async () => {
     const ctaDetailsText = ctaDetails.textContent
 
     const newDesign = document.querySelector('#new-design')
-    const generalsTab = newDesign.querySelector('[data-w-tab="Tab 3"]')
-    const recommendationsTab = newDesign.querySelector('[data-w-tab="Tab 2"]')
-    const toursTab = newDesign.querySelector('[data-w-tab="Tab 1"]')
-
-    toursTab.click()
-
-    const truncateString = (str, maxLength) => {
-        return str.length <= maxLength ? str : str.slice(0, maxLength - 3) + '...';
-    }
+    const [toursTab, recommendationsTab, generalsTab] = [1, 2, 3]
+        .map(dwt => newDesign
+            .querySelector(`[data-w-tab="Tab ${dwt}"]`))
 
     const removeCtaWrapper = () => {
         newDesign.querySelector('.guide-cta-wrapper').remove()
     }
+    toursTab.click()
 
     const runTabFunctions = () => {
         const tabSection = document.querySelector('.tabs-content.w-tab-content');
         const compendium = JSON.parse(compendiumText)
 
-        const generalDataArray = compendium.general.map(gen => {
-            return {
-                iconDetails: gen.iconDetails,
-                active: gen.active,
-                accordionTitle: gen.title,
-                media: gen.media
-            }
+        const generalDataArray = compendium.general.map(({ iconDetails, active, title, media }) =>
+            ({ iconDetails, active, accordionTitle: title, media })
+        )
+
+        const recomDataArray = compendium.recommendations.map(({ iconDetails, active, title, media }) =>
+        ({
+            iconDetails,
+            active,
+            accordionTitle: title,
+            media: media.map(({ current_opening_hours, active, overview, content, name, phoneNumber, website, mapsUrl, cardImg }) =>
+                ({ opening_hours: current_opening_hours, active, overview, placeId: content, name, phoneNumber, website, mapsUrl, cardImg })
+            )
         })
+        )
 
-
-        const recomDataArray = compendium.recommendations.map((rec) => {
-            return {
-                iconDetails: rec.iconDetails,
-                active: rec.active,
-                accordionTitle: rec.title,
-                media: rec.media.map(({ current_opening_hours, active, overview, content, name, phoneNumber, website, mapsUrl, cardImg }) =>
-                    ({ opening_hours: current_opening_hours, active, overview, placeId: content, name, phoneNumber, website, mapsUrl, cardImg })
-                )
-            }
-        })
-
-        const validateRecomData = recomDataArray.filter(rda => rda.active && rda.media.filter(rd => rd.active).length > 1).length > 1
-        const validateGeneralData = generalDataArray.filter(gda => gda.active).length > 1
-
-        if (!validateRecomData) {
-            recommendationsTab.remove()
-        }
-        if (!validateGeneralData) {
-            generalsTab.remove()
-        }
-
-        // Let's scope this in a block so it wouldn't interfere with other tabs
-        const runRecommendations = () => {
-            const recomTab = tabSection.querySelector('div#w-tabs-0-data-w-pane-1');
-            const recomDropdownWrapper = recomTab.querySelector('.guide-dropdown-list-wrapper.recommendation');
-            const recomDropdownTemplate = recomDropdownWrapper.querySelector('.guide-accordion-item.w-dropdown');
-            const recomCardWrapperTemplate = recomDropdownTemplate.querySelector('.recommendation-card-wrapper');
-
+        const tabsGenericFn = () => {
             const closeToggle = ({ toggleBody, accordionBody, accordionBtn, clone }) => {
                 toggleBody.classList.remove('w--open')
                 accordionBody.classList.remove('w--open')
@@ -113,9 +92,9 @@ const runFn = async () => {
                 clone.style.height = '30px'
             }
 
-            const switchAccordionBody = (toggle) => {
+            const switchAccordionBody = ({ toggle, wrapper }) => {
                 // This function helps avoid multiple opened accordion body
-                const accorBodiesNode = recomDropdownWrapper.querySelectorAll('.accordion-toggle.w-dropdown-toggle')
+                const accorBodiesNode = wrapper.querySelectorAll('.accordion-toggle.w-dropdown-toggle')
                 const accorBodies = Array.from(accorBodiesNode)
 
                 accorBodies.filter(ab => ab.id !== toggle.id).forEach((toggleBody) => {
@@ -127,20 +106,7 @@ const runFn = async () => {
                 })
             }
 
-
-            recomDropdownTemplate.remove()
-            recomCardWrapperTemplate.remove()
-            // Iterate over the data array and clone the template for each item
-            recomDataArray.filter(rda => rda.active && rda.media.filter(rd => rd.active).length > 1).forEach((data, idx) => {
-                // Clone the template
-                const recomDropdownClone = recomDropdownTemplate.cloneNode(true);
-
-                const toggle = recomDropdownClone.querySelector('.accordion-toggle.w-dropdown-toggle')
-                const accordionBody = recomDropdownClone.querySelector('nav.accordion-body.w-dropdown-list')
-                const accordionBtn = recomDropdownClone.querySelector('.accordion-btn')
-                // const recomCardWrapperTemplate = recomDropdownClone.querySelector('.recommendation-card-wrapper')
-                const cardLayout = recomDropdownClone.querySelector('.recommendation-card-layout')
-
+            const toggleFn = ({ toggle, accordionBody, accordionBtn, clone, wrapper, idx }) => {
                 const toggleId = `w-dropdown-toggle-${idx + 1}`
                 const dropdownId = `w-dropdown-list-${idx + 1}`
 
@@ -156,21 +122,54 @@ const runFn = async () => {
                         accordionBody.classList.add('w--open')
                         toggle.setAttribute('aria-expanded', 'true')
                         accordionBtn.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(180deg) skew(0deg, 0deg)'
-                        recomDropdownClone.style.zIndex = '901'
-                        recomDropdownClone.style.height = ''
+                        clone.style.zIndex = '901'
+                        clone.style.height = ''
 
-                        switchAccordionBody(toggle)
+                        switchAccordionBody({ toggle, wrapper })
                     } else {
-                        closeToggle({ toggleBody: toggle, accordionBody, accordionBtn, clone: recomDropdownClone })
+                        closeToggle({ toggleBody: toggle, accordionBody, accordionBtn, clone })
                     }
                 })
+            }
+
+            return { toggleFn }
+        }
+
+        // Let's scope this in a block so it wouldn't interfere with other tabs
+        const runRecommendations = () => {
+            // Let's validate if there are any data to be rendered otherwise, let's just remove the tab and not run the function
+            const validateRecomData = recomDataArray.filter(rda => rda.active && rda.media.filter(rd => rd.active).length > 1).length > 1
+            if (!validateRecomData) {
+                recommendationsTab.remove()
+                return
+            }
+
+            const recomTab = tabSection.querySelector('div#w-tabs-0-data-w-pane-1');
+            const recomDropdownWrapper = recomTab.querySelector('.guide-dropdown-list-wrapper.recommendation');
+            const recomDropdownTemplate = recomDropdownWrapper.querySelector('.guide-accordion-item.w-dropdown');
+            const recomCardWrapperTemplate = recomDropdownTemplate.querySelector('.recommendation-card-wrapper');
+            const { toggleFn } = tabsGenericFn()
+
+            recomDropdownTemplate.remove()
+            recomCardWrapperTemplate.remove()
+            // Iterate over the data array and clone the template for each item
+            recomDataArray.filter(rda => rda.active && rda.media.filter(rd => rd.active).length > 1).forEach((data, idx) => {
+                // Clone the template
+                const recomDropdownClone = recomDropdownTemplate.cloneNode(true);
+
+                const toggle = recomDropdownClone.querySelector('.accordion-toggle.w-dropdown-toggle')
+                const accordionBody = recomDropdownClone.querySelector('nav.accordion-body.w-dropdown-list')
+                const accordionBtn = recomDropdownClone.querySelector('.accordion-btn')
+                const cardLayout = recomDropdownClone.querySelector('.recommendation-card-layout')
+
+                toggleFn({ accordionBody, accordionBtn, clone: recomDropdownClone, toggle, wrapper: recomDropdownWrapper, idx })
 
                 // Update content with data from the array
                 recomDropdownTemplate.setAttribute('data-w-id', generateUUID())
                 recomDropdownClone.querySelector('[recom-data="accordion-title"]').textContent = data.accordionTitle;
                 recomDropdownClone.querySelector(`[recom-data="media-icon"]`).setAttribute('src', data.iconDetails.url)
 
-                data.media.filter(med => med.active).forEach((medData, medIdx) => {
+                data.media.filter(med => med.active).forEach((medData) => {
                     const mediaClone = recomCardWrapperTemplate.cloneNode(true);
                     const writeMedia = (dataAttr, text, attr = 'textContent') => {
                         mediaClone.querySelector(`[recom-data="${dataAttr}"]`)[attr] = text
@@ -235,32 +234,17 @@ const runFn = async () => {
 
         // Let's scope this in a block so it wouldn't interfere with other tabs
         const runGeneral = () => {
+            // Let's validate if there are any data to be rendered otherwise, let's just remove the tab and not run the function
+            const validateGeneralData = generalDataArray.filter(gda => gda.active).length > 1
+            if (!validateGeneralData) {
+                generalsTab.remove()
+                return
+            }
+
             const genTab = tabSection.querySelector('div#w-tabs-0-data-w-pane-2');
             const genDropdownWrapper = genTab.querySelector('.guide-dropdown-list-wrapper.general');
             const genDropdownTemplate = genDropdownWrapper.querySelector('.guide-accordion-item.w-dropdown');
-
-            const closeToggle = ({ toggleBody, accordionBody, accordionBtn, clone }) => {
-                toggleBody.classList.remove('w--open')
-                accordionBody.classList.remove('w--open')
-                toggleBody.setAttribute('aria-expanded', 'false')
-                accordionBtn.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)'
-                clone.style.zIndex = ''
-                clone.style.height = '30px'
-            }
-
-            const switchAccordionBody = (toggle) => {
-                // This function helps avoid multiple opened accordion body
-                const accorBodiesNode = genDropdownWrapper.querySelectorAll('.accordion-toggle.w-dropdown-toggle')
-                const accorBodies = Array.from(accorBodiesNode)
-
-                accorBodies.filter(ab => ab.id !== toggle.id).forEach((toggleBody) => {
-                    const accorClone = toggleBody.parentNode
-                    const accordionBody = accorClone.querySelector('nav.accordion-body.w-dropdown-list')
-                    const accordionBtn = accorClone.querySelector('.accordion-btn')
-
-                    closeToggle({ toggleBody, accordionBody, accordionBtn, clone: accorClone })
-                })
-            }
+            const { toggleFn } = tabsGenericFn()
 
             genDropdownTemplate.remove()
 
@@ -273,31 +257,7 @@ const runFn = async () => {
                 const accordionBody = genDropdownClone.querySelector('nav.accordion-body.w-dropdown-list')
                 const accordionBtn = genDropdownClone.querySelector('.accordion-btn')
 
-                const toggleId = `w-dropdown-toggle-${idx + 1}`
-                const dropdownId = `w-dropdown-list-${idx + 1}`
-
-                toggle.setAttribute('id', toggleId)
-                toggle.setAttribute('aria-controls', dropdownId)
-                accordionBody.setAttribute('id', dropdownId)
-                accordionBody.setAttribute('aria-labelledby', toggleId)
-                accordionBtn.style.transition = '0.5s'
-
-                toggle.addEventListener('click', () => {
-                    if (!toggle.classList.contains('w--open')) {
-                        toggle.classList.add('w--open')
-                        accordionBody.classList.add('w--open')
-                        toggle.setAttribute('aria-expanded', 'true')
-                        accordionBtn.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(180deg) skew(0deg, 0deg)'
-                        genDropdownClone.style.zIndex = '901'
-                        genDropdownClone.style.height = ''
-
-                        switchAccordionBody(toggle)
-                    } else {
-                        closeToggle({ toggleBody: toggle, accordionBody, accordionBtn, clone: genDropdownClone })
-                    }
-
-
-                })
+                toggleFn({ accordionBody, accordionBtn, toggle, clone: genDropdownClone, wrapper: genDropdownWrapper, idx })
 
                 // Update content with data from the array
                 genDropdownTemplate.setAttribute('data-w-id', generateUUID())
@@ -321,37 +281,41 @@ const runFn = async () => {
                     video: genMediaVideoTemplate
                 }
 
-                data.media.forEach((medData, medIdx) => {
-                    if (!['text', 'link', 'img', 'video'].includes(medData.type)) {
-                        return // Let's skip first if there are more types than the 4
+                const mediaFn = ({ data, clone }) => {
+                    const { content, title } = data
+                    return {
+                        text: () => {
+                            clone.textContent = content
+                        },
+                        link: () => {
+                            const link = clone.querySelector('[gen-data="media-link"]')
+                            link.setAttribute('href', content)
+                            link.setAttribute('target', '_blank')
+                            link.textContent = title || link.textContent
+                        },
+                        img: () => {
+                            const img = clone.querySelector('[gen-data="media-img"]')
+                            img.setAttribute('src', content)
+                            img.setAttribute('srcset', '')
+                        },
+                        video: () => {
+                            const video = clone.querySelector('[gen-data="media-vid"]')
+                            video.querySelector('iframe.embedly-embed').setAttribute('src', content)
+                        },
                     }
+                }
 
-                    const mediaClone = elementWithType[medData.type].cloneNode(true);
+                data.media.forEach((data) => {
+                    if (!Object.keys(elementWithType).includes(data.type)) return // Let's skip first if there are more types than the 4
 
-                    if (medData.type === 'text') {
-                        mediaClone.textContent = medData.content
-                    }
-                    else if (medData.type === 'link') {
-                        const link = mediaClone.querySelector('[gen-data="media-link"]')
-                        link.setAttribute('href', medData.content)
-                        link.setAttribute('target', '_blank')
-                        link.textContent = medData?.title || link.textContent
-                    }
-                    else if (medData.type === 'img') {
-                        const img = mediaClone.querySelector('[gen-data="media-img"]')
-                        img.setAttribute('src', medData.content)
-                        img.setAttribute('srcset', '')
-                    } else if (medData.type === 'video') {
-                        const video = mediaClone.querySelector('[gen-data="media-vid"]')
-
-                        video.querySelector('iframe.embedly-embed').setAttribute('src', medData.content)
-                    }
-
-                    genDropdownClone.querySelector('.accordion-body-content').append(mediaClone)
+                    const clone = elementWithType[data.type].cloneNode(true)
+                    mediaFn({ data, clone })[data.type]() // Let's run the function based on the `media.type`
+                    genDropdownClone
+                        .querySelector('.accordion-body-content')
+                        .append(clone)
                 })
 
-                // // Append the cloned element to the wrapper
-                genDropdownWrapper.append(genDropdownClone);
+                genDropdownWrapper.append(genDropdownClone); // Append the cloned element to the wrapper
             });
         }
 
@@ -359,6 +323,7 @@ const runFn = async () => {
         runGeneral()
     }
 
+    // Update Profile Section using details from CTA field in Guide Collections
     const runProfileFunctions = () => {
         const ctaLink = newDesign.querySelector('[profile-data="cta-link"]')
         const ctaMobile = newDesign.querySelector('[profile-data="cta-mobile"]')
@@ -396,7 +361,8 @@ const runFn = async () => {
 startObservingElements({
     selectors: [
         'div#w-tabs-0-data-w-pane-1',
-        'code#json-compendium'
+        'code#json-compendium',
+        'code#json-cta'
     ],
     callback: runFn
 });
