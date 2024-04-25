550e7ee127ed98317fbd2c0ed48cff59870e673f
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-04-25 16:59:51 +0800

    chore(): rewrite img removal using display to none

--- prod/compendium.js
+++ prod/compendium.js
@@ -245,17 +245,17 @@ const runFn = async () => {
                     } else {
                         const imgs = medData.cardImg
 
-                        if (imgs.length === 0) {
-                            cardimageWrapper.remove()
-                            return
-                        }
-
                         imgs.forEach((i, idx) => {
                             writeMedia(`media-card-img-${idx + 1}`, i || '', 'src')
                         })
 
                         if (imgs.length < 3) {
-
+                            if (imgs.length === 0) {
+                                for (let i = 0; i < newLength; i++) {
+                                    mediaClone.querySelector(`[recom-data="media-card-img-${3 - i}"]`).style.display = 'none'
+                                }
+                                return
+                            }
                             const newLength = 3 - imgs.length
 
                             for (let i = 0; i < newLength; i++) {
