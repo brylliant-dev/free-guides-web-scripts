589b99e5a62230a50c7d8ce17060e980b7156d47
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-06-04 00:20:38 +0800

    chore(): remove id for query selectors

--- prod/compendium.js
+++ prod/compendium.js
@@ -55,7 +55,7 @@ const runOldFn = async () => {
   const ctaDetails = document.querySelector('code#json-cta')
   const ctaDetailsText = ctaDetails.textContent
 
-  const mainWrapper = document.querySelector('#old-design.main-wrapper')
+  const mainWrapper = document.querySelector('.main-wrapper')
   const allTabs = [1, 2, 3].map((dwt) =>
     mainWrapper.querySelector(`[data-w-tab='Tab ${dwt}']`)
   )
@@ -521,7 +521,7 @@ const runNewFn = async () => {
   const ctaDetails = document.querySelector('code#json-cta')
   const ctaDetailsText = ctaDetails.textContent
 
-  const mainWrapper = document.querySelector('#new-design.main-wrapper')
+  const mainWrapper = document.querySelector('.main-wrapper')
   const allTabs = [1, 2, 3].map((dwt) =>
     mainWrapper.querySelector(`[data-w-tab='Tab ${dwt}']`)
   )
