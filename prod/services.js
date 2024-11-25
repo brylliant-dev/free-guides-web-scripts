b0f851902973903232a07086e5bc69f460ea0787
Synapse96 <rajsharma96@hotmail.com>
2024-11-25 14:19:35 +1100

    deep clone of tab

--- prod/services.js
+++ prod/services.js
@@ -7,15 +7,15 @@ $(document).ready(() => {
       var tabHeader = document.querySelector('.tour-guide-heading-wrapper')
       var l = tabHeader.children.length
       const { category, value, url, enabled } = p 
-      tabHeader.appendChild(tabHeader.children[0])
-      var newTab = tabHeader.children[l]
+      var newTab = tabHeader.children[0].cloneNode()
       newTab.setAttribute('id',category + '-tab-button')
       newTab.setAttribute('data-w-tab','Tab ' + l)
       newTab.setAttribute('data-tab-name',category)
       newTab.setAttribute('aria-controls','w-tabs-0-data-w-pane-' + l)
-      newTab.innerHTML = ``
+      newTab.innerHTML = ""
       console.log(newTab)
 
+      tabHeader.appendChild(newTab)
       console.log(tabHeader.children)
     }
 
