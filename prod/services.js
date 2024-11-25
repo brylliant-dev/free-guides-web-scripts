369879aa9592472bf8998ee869d07a0ad3ad66e7
Synapse96 <rajsharma96@hotmail.com>
2024-11-25 12:44:41 +1100

    console messages

--- prod/services.js
+++ prod/services.js
@@ -11,9 +11,10 @@ $(document).ready(() => {
       newTab.setAttribute('data-w-tab','Tab ' + l)
       newTab.setAttribute('data-tab-name',category)
       newTab.setAttribute('aria-controls','w-tabs-0-data-w-pane-' + l)
+      console.log(newTab)
 
-      tabHeader.appendChild(newTab)
-      console.log(tabHeader.children)
+      //tabHeader.appendChild(newTab)
+      //console.log(tabHeader.children)
     }
 
     const runServicesFn = () => {
