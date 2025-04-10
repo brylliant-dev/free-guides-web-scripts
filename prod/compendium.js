fe15cf1ec4c4176dc16931eb5e786c18101e8d1b
Synapse96 <rajsharma96@hotmail.com>
2025-04-10 15:04:14 +1000

    small increase to place card size

--- prod/compendium.js
+++ prod/compendium.js
@@ -278,7 +278,7 @@ const runFn = async () => {
               const placeId = medData.placeId
               const details = medData.details ?? ''
 
-              mediaClone.innerHTML = `<iframe src='' height='380px' width='100%' loading='lazy' class='iFrame1'></iframe>`
+              mediaClone.innerHTML = `<iframe src='' height='395px' width='100%' loading='lazy' class='iFrame1'></iframe>`
 
               iframeClones.push({
                 item: mediaClone.querySelector('iframe'),
