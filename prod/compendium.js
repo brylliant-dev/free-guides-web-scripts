b21cacaed84b691456b126ef92c3e6e15b7eb029
Synapse96 <rajsharma96@hotmail.com>
2025-05-01 11:17:40 +1000

    trying to fix text font issue

--- prod/compendium.js
+++ prod/compendium.js
@@ -370,6 +370,7 @@ const runFn = async () => {
               text: () => {
                 clone.style.fontFamily = stringBrandFont
                 clone.innerHTML = content
+                clone.children[0].style.fontFamily = stringBrandFont
               },
               link: () => {
                 const link = clone.querySelector('[gen-data="media-link"]')
